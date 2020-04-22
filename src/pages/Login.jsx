abaa0fedb1481eefa6f40a8a8e740a1523113d4d
Marek <marekw1996@gmail.com>
2020-04-22 22:31:55 +0200

    added changes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -24,11 +24,12 @@ const onChange = ({ target }) => {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
+        'Accept': 'application/json',
       },
       body: JSON.stringify({ email, password })
     });
     setLoading(false);
-    console.log('res: ', res);
+    console.log('res: ', res.headers);
     if(res.status > 300 ) return;
     history.push('/home');
     } catch(error) {
